67688088ca852eb0cd588506c544e1fd14a0caa5
Arush Singh <sarushmad@gmail.com>
2024-06-25 09:55:59 +0530

    updated nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Link = [
+  {
+    name: "home",
+    path: "/"
+  },
+  {
+    name: "services",
+    path: "/services"
+  },
+  {
+    name: "brouche",
+    path: "/brouche"
+  },
+  {
+    name: "work",
+    path: "/work"
+  },
+  {
+    name: "contact",
+    path: "/contact"
+  },
+]
+
+
 const Nav = () => {
-  return <nav>desktop nav</nav>;
+  return ( 
+  <nav className="flex gap-8">
+    {link.map((link, index) => {
+      return (
+      <Link href={link.path} key={index}>
+        {link.name}
+        </Link>
+      );
+    })}
+  </nav>
+  );
 };
 
 export default Nav;
