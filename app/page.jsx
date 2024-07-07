bb1573a46bc3c7b07ddb246df0539dfd9b81436c
Arush Singh <sarushmad@gmail.com>
2024-07-07 10:04:32 +0530

    completed landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Photo from "@/components/Photo";
 import Social from "@/components/Social";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi" 
@@ -13,7 +14,7 @@ const Home = () => {
           <h1 className="h1 mb-6">
             Hello We're<br /><span className="text-accent">Educare Services</span>
           </h1>
-          <p className="max-w-[500px] mb-9 text-white/80">We are dedicated to provide personalized guidance and support to undergraduates and postgraduates programs such as medical (MBBS, BDS, BAMS, BHMS, & BUMS) and all other branches of engineering, commerce, banking, management and so on. Helping them achieve academic success and reach their full potential. With a team of experienced consultants, we offer expertise in college admissions, and career guidance. Our mission is to empower students through tailored advice, resources, and support, and preparing them for future success. We are committed to professionalism, ethical practices, and continuous improvement to ensure that every student receives the best possible educational consultancy experience.
+          <p className="max-w-[500px] mb-9 text-white/80">We are dedicated to provide personalized guidance and support to undergraduates and postgraduates.
           </p>
           {/* buttons and socials */}
           <div className="flex flex-col xl:flex-row items center gap-8">
@@ -26,12 +27,17 @@ const Home = () => {
               <FiDownload className="text-xl"/>
             </Button>
             <div className="mb-8 xl:mb-0">
-              <Social />
+              <Social 
+              containerStyles= "flex gap-6" 
+              iconStyles= "w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" 
+              />
               </div>
           </div>
         </div>
         {/*photo */}
-        <div>photo</div>
+        <div>
+          <Photo />
+        </div>
       </div>
     </div>
     </section>
--- components/Social.jsx
+++ components/Social.jsx
@@ -1,7 +1,25 @@
-const Social = () => {
+import Link from "next/link";
+
+import { FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  {icon: <FaLinkedinIn />, path: ""},
+  {icon: <FaTwitter />, path: ""},
+  {icon: <FaYoutube />, path: ""},
+];
+
+const Social = ({containerStyles, iconStyles}) => {
   return (
-    <div>Social</div>
-  )
-}
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return ( 
+          <Link key={index} href={item.path} className={iconStyles}>
+          {item.icon}
+          </Link>
+          );
+      })}
+    </div>
+  );
+};
 
 export default Social;
