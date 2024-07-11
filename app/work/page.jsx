cb2f4d1ac7406a031ee90853f98038fb6714454b
Arush Singh <sarushmad@gmail.com>
2024-07-11 13:56:13 +0530

    added responsiveness in student cards

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -49,12 +50,39 @@ const students = [
   },
 ]
 
-const settings = {
+var settings = {
   dots: true,
   infinite: true,
   speed: 500,
   slidesToShow: 3,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 };
 
 const Work = () => {
