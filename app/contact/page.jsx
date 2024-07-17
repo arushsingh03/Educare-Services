10dae95f072f9c656249116c9beb9c5e6243d9db
Arush Singh <sarushmad@gmail.com>
2024-07-17 15:17:01 +0530

    updated scripts

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,12 @@
 "use client";
+import Script from 'next/script';
 
 import React from 'react'
 
 const contact = () => {
   return (
     <>
-      <div className="visme_d" data-title="Feedback Form" data-url="y4vw36xj-feedback-form" data-domain="forms" data-full-page="false" data-min-height="500px" data-form-id="83098"></div><script src="https://static-bundles.visme.co/forms/vismeforms-embed.js"></script>
+      <div className="visme_d" data-title="Feedback Form" data-url="y4vw36xj-feedback-form" data-domain="forms" data-full-page="false" data-min-height="500px" data-form-id="83098"></div><Script src="https://static-bundles.visme.co/forms/vismeforms-embed.js"></Script>
     </>
   );
 };
